b6207dbd1eadfa1e057ace4411a8ea35fa763f0d
Rawin D <rawin.dbc@outlook.com>
2017-11-03 20:30:21 +0700

    end day1

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import UI from  './src/p2/ui.js';
+import { StackNavigator } from "react-navigation";
 
 export default class App extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+  }
   render() {
     return (
+        <View style={{height=200}}/>
         <View style={styles.container}>
-            <UI/>
+            <Text style={styles.textSlide}>Slide to next page &lt;</Text>
         </View>
     );
   }
 }
 
+const ModalStack = StackNavigator({
+  Home: {
+    screen: UI,
+  },
+});
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      width: '100%',
+      backgroundColor: '#555',
+      alignItems: 'center',
+      //justifyContent: 'center',
+      padding: 20
+  },
+  textSlide: {
+      color: "#ccc", fontSize: 24,
+      marginTop:20
   },
 });
